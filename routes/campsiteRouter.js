53c952cac814d43b700def3d0a3e3205f68fcdab
Gustavo Castaneda <gustavocastaneda2323@gmail.com>
2020-11-21 12:21:28 -0800

    Workshop Week 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,24 +1,50 @@
-const express = require('express');
+const express = require("express");
 const campsiteRouter = express.Router();
 
-campsiteRouter.route('/')
-.all((req, res, next) => {
+campsiteRouter
+  .route("/")
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
     next();
-})
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
-})
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
+  })
+  .get((req, res) => {
+    res.end("Will send all the campsites to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-})
-.delete((req, res) => {
-    res.end('Deleting all campsites');
-});
+    res.end("PUT operation not supported on /campsites");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all campsites");
+  });
+
+campsiteRouter
+  .route("/:campsiteId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the campsites/campsiteId to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the campsite/campsiteId: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /campsites/campsiteId");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all campsites");
+  });
 
 module.exports = campsiteRouter;
